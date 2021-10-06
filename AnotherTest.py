19ba42d1f22e31916b70d8f1cd63e442dc1e26a4
CedricStandaert <65156295+Krakboem@users.noreply.github.com>
2021-10-06 21:58:21 +0200

    Update AnotherTest.py

--- AnotherTest.py
+++ AnotherTest.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 print("This is another test")
 
@@ -6,3 +7,6 @@ cwd = os.getcwd()
 
 print("Current working directory: {0}".format(cwd))
 print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
+
+df = pd.read_csv ("C:\Users\MA642GN\OneDrive - EY\Documents\Microsoft Azure\DevOps\Data Kaggle\home-credit-default-risk\application_test.csv")
+print(df)
