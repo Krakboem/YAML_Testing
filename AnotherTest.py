74ff1e580ed0b5b2e4ff9b9e8d748f30a4730c69
CedricStandaert <65156295+Krakboem@users.noreply.github.com>
2021-10-06 21:54:25 +0200

    Update AnotherTest.py

--- AnotherTest.py
+++ AnotherTest.py
@@ -1,14 +1,8 @@
-print("This is another test")
-
-
-# Import the os module
 import os
 
-# Get the current working directory
+print("This is another test")
+
 cwd = os.getcwd()
 
-# Print the current working directory
 print("Current working directory: {0}".format(cwd))
-
-# Print the type of the returned object
 print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
